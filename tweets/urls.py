11e1287d8acc832d3fa8d88850f5751cdaf0974c
amir <amirnurani2001@gmail.com>
2023-04-20 21:48:17 +0330

    creating tweetdetail_view and it's route in urls.py file

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.home_view, name="home View")
+    path("", views.home_view, name="home View"),
+    path("tweets/<int:tweet_id>", views.tweetdetail_view, name="tweet detail View"),
 ]
