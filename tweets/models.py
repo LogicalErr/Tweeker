1a078330a8d62826f6a4ca6a9c533d7c4efb8b4a
amir <amirnurani2001@gmail.com>
2023-05-10 02:14:39 +0330

    add a user for every tweet

--- tweets/models.py
+++ tweets/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 import random
+
 
 # Create your models here.
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # one-to-many a user can have many tweets
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
     
