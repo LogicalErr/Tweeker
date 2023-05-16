8b0d46b68ad0be89511f52e051e823b6ef08e5f0
amir <amirnurani2001@gmail.com>
2023-05-16 15:01:57 +0330

    set static root for static files

--- backend/urls.py
+++ backend/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from tweets.views import home_view
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", home_view, name="home view"),
-    path('api/tweets/', include("tweets.urls"))
+    path('api/tweets/', include("tweets.urls")),
+    path("react/",TemplateView.as_view(template_name= 'react.html')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
